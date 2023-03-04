ea62e252bbbce643c4a0009760934308d376a5fa
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-03-05 02:33:11 +0600

    added city name in h1 tag

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 const API_KEY = 'b4673fec0705f8956abc5e4fa2e8283c'
-const city = 'dhaka'
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
 
-fetch(url)
-    .then(res => res.json())
-    .then(data => console.log(data))
+document.getElementById('search-btn').addEventListener('click', () => {
+    const cityValue = document.getElementById('city').value
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${API_KEY}`
+    console.log(cityValue)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showInfo(data))
+})
+function setValueById(id, info){
+    if(info){
+        document.getElementById(id).innerHTML = info
+    }
+    else{
+        document.getElementById(id).innerHTML = 'Please enter a valid city'
+    }
+}
+
+const showInfo = data => {
+    console.log(data.name)
+    setValueById('cityName', data.name)
+}
